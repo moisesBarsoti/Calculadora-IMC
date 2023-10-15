b147c15352e4391351c17d40094248ed3c7d9704
moisesBarsoti <146322015+moisesBarsoti@users.noreply.github.com>
2023-10-15 18:03:48 -0300

    Subindo Arquivos

--- calculadoraIMC/assets/js/script.js
+++ calculadoraIMC/assets/js/script.js
@@ -3,22 +3,41 @@ function calcularIMC() {
 
     botao.addEventListener('click', function(e) {
         e.preventDefault();
-         let peso = document.querySelector('#peso');
-         let altura = document.querySelector('#altura');
-         const resultadoNoNavegador = document.querySelector('#resultado');
+        let peso = document.querySelector('#peso');
+        let altura = document.querySelector('#altura');
+        const resultadoNoNavegador = document.querySelector('#resultado');
         
         let contaIMC = peso.value / (altura.value * altura.value);
+        console.log(contaIMC)
 
-        if(contaIMC < 18,5) {
+        if (contaIMC >= 40) {
             resultadoNoNavegador.innerHTML = `
-              ${contaIMC.toFixed(1)} Está abaixo do peso.
+            ${contaIMC.toFixed(1)} obesidade grau 3 <br>
             `;
-        } else if(contaIMC >= 18,5) {
+            return;
+        } else if(contaIMC >= 35) {
             resultadoNoNavegador.innerHTML += `
-            ${contaIMC.toFixed(1)} Está com o peso normal.
-            `
-        }
-    });
+            ${contaIMC.toFixed(1)} obesidade grau 2 <br>
+            `;
+        } else if(contaIMC >= 30) {
+            resultadoNoNavegador.innerHTML += `
+            ${contaIMC.toFixed(1)} obesidade grau 1 <br>
+            `;
+        } else if(contaIMC >= 25) {
+            resultadoNoNavegador.innerHTML += `
+            ${contaIMC.toFixed(1)} Sobrepeso <br>
+            `;
+        } else if(contaIMC >= 18.5) {
+            resultadoNoNavegador.innerHTML += `
+            ${contaIMC.toFixed(1)} Peso normal <br>
+            `;
+        }    
+          else if(contaIMC < 18.5) {
+            resultadoNoNavegador.innerHTML += `
+            ${contaIMC.toFixed(1)} Abaixo do peso <br>
+            `;
+        }    
+});
 };
 
 calcularIMC()
